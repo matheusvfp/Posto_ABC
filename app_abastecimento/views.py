67957e9cb8ab5892316853f3926b903a8a45cb5d
Matheus Vinicius <matheusv83200@gmail.com>
2024-07-24 19:52:48 -0300

    ✨ feat: cadastro de abastecimento

--- app_abastecimento/views.py
+++ app_abastecimento/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 
-from .models import Abastecimento
+from .models import Abastecimento, Bomba, Tanque
 
 
 class AbastecimentosListView(ListView):
     model = Abastecimento
+
+class AbastecimentoCreateView(CreateView):
+    model = Abastecimento
+    fields = ["bomba", "quantidade_litros", "valor_abastecido"]
+    success_url = reverse_lazy("abastecimento_list")
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from app_abastecimento.views import AbastecimentosListView
+from app_abastecimento.views import AbastecimentosListView, AbastecimentoCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("listar/", AbastecimentosListView.as_view()),
+    path("", AbastecimentoCreateView.as_view(), name="abastecimento_create"),
+    path("listar/", AbastecimentosListView.as_view(), name="abastecimento_list"),
 ]
