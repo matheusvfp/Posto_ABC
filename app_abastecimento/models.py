86f8b273bbaf667d4ad61221f2c268ae7a978237
Matheus Vinicius <matheusv83200@gmail.com>
2024-07-24 10:53:52 -0300

    ✨ feat: fomrtação do projeto com black

--- app_abastecimento/models.py
+++ app_abastecimento/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Tanque(models.Model):
     TIPO_COMBUSTIVEL = [
-        ('GASOLINA', 'Gasolina'),
-        ('DIESEL', 'Óleo Diesel'),
+        ("GASOLINA", "Gasolina"),
+        ("DIESEL", "Óleo Diesel"),
     ]
-    
-    tipo_combustivel = models.CharField(max_length=50, choices=TIPO_COMBUSTIVEL)  
+
+    tipo_combustivel = models.CharField(max_length=50, choices=TIPO_COMBUSTIVEL)
     data = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.tipo_combustivel
+        return self.tipo_combustive
