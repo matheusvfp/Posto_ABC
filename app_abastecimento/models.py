ca37347779a4ba6c0533c0eeaa7227493640e981
Matheus Vinicius <matheusv83200@gmail.com>
2024-07-24 10:10:15 -0300

    ✨ feat: criacão do modelo de tanque

--- app_abastecimento/models.py
+++ app_abastecimento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Tanque(models.Model):
+    TIPO_COMBUSTIVEL = [
+        ('GASOLINA', 'Gasolina'),
+        ('DIESEL', 'Óleo Diesel'),
+    ]
+    
+    tipo_combustivel = models.CharField(max_length=50, choices=TIPO_COMBUSTIVEL)  
+    data = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.tipo_combustivel
