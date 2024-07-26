3bf08485c9b92ddedaca659e3833912a19c34bd8
Matheus Vinicius <matheusv83200@gmail.com>
2024-07-26 16:16:26 -0300

    ♻️  refactor: ajuste de variáveis

--- setup/urls.py
+++ setup/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", AbastecimentoCreateView.as_view(), name="abastecimento_create"),
     path("listar/", AbastecimentosListView.as_view(), name="abastecimento_list"),
-    path("criar-tanques/", TanqueCreateView.as_view(), name="criar_tanques"),
-    path("criar-bombas/", BombaCreateView.as_view(), name="criar_bombas"),
+    path("criar-tanques/", TanqueCreateView.as_view(), name="tanques_create"),
+    path("criar-bombas/", BombaCreateView.as_view(), name="bombas_create"),
 ]
