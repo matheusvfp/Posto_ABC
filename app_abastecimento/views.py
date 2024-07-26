da9f040bb6f070972650e145a65b8dde7d4008c9
Matheus Vinicius <matheusv83200@gmail.com>
2024-07-26 10:22:40 -0300

    ✨ feat: criação dos tanques e bombas

--- app_abastecimento/views.py
+++ app_abastecimento/views.py
@@ -1,12 +1,43 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, View
 from django.urls import reverse_lazy
-
-
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Abastecimento, Bomba, Tanque
+
+
+class TanqueCreateView(View):
+    def get(self, request):
+        if not Tanque.objects.exists():
+            Tanque.objects.create(tipo_combustivel="GASOLINA")
+            Tanque.objects.create(tipo_combustivel="DIESEL")
+            messages.success(request, "Tanques criados com sucesso!")
+        else:
+            messages.info(request, "Tanques já existem.")
+        return HttpResponseRedirect(reverse_lazy("abastecimento_create"))
+
+
+class BombaCreateView(View):
+    def get(self, request):
+        try:
+            if not Bomba.objects.exists():
+                gasolina = Tanque.objects.get(tipo_combustivel="GASOLINA")
+                diesel = Tanque.objects.get(tipo_combustivel="DIESEL")
+                Bomba.objects.create(tanque=gasolina, bomba_utilizada="GASOLINA 1")
+                Bomba.objects.create(tanque=gasolina, bomba_utilizada="GASOLINA 2")
+                Bomba.objects.create(tanque=diesel, bomba_utilizada="DIESEL 1")
+                Bomba.objects.create(tanque=diesel, bomba_utilizada="DIESEL 2")
+                messages.success(request, "Bombas criadas com sucesso!")
+            else:
+                messages.info(request, "Bombas já existem.")
+        except Tanque.DoesNotExist:
+            messages.error(request, "Erro: É necessário criar os tanques primeiro.")
+        return HttpResponseRedirect(reverse_lazy("abastecimento_create"))
 
 
 class AbastecimentosListView(ListView):
     model = Abastecimento
+    queryset = Abastecimento.objects.all()
+
 
 class AbastecimentoCreateView(CreateView):
     model = Abastecimento
--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from app_abastecimento.views import AbastecimentosListView, AbastecimentoCreateView
+from app_abastecimento.views import (
+    AbastecimentosListView,
+    AbastecimentoCreateView,
+    BombaCreateView,
+    TanqueCreateView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", AbastecimentoCreateView.as_view(), name="abastecimento_create"),
     path("listar/", AbastecimentosListView.as_view(), name="abastecimento_list"),
+    path("criar-tanques/", TanqueCreateView.as_view(), name="criar_tanques"),
+    path("criar-bombas/", BombaCreateView.as_view(), name="criar_bombas"),
 ]
